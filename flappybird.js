9de4092d5492f01b99d8cf01bd9a42ffac53cb13
avinebel <73241397+avinebel@users.noreply.github.com>
2021-02-12 14:27:47 -0500

    Add files via upload

--- flappybird.js
+++ flappybird.js
@@ -26,13 +26,10 @@ function create() {
 
     const poles = this.physics.add.group();
     function createPole() {
-      //how to make it with holes, canvas height of 620
-      //i think the holes should be 200 gap
         function getYPos(min, max) {
           return Math.random() * (max - min) + min;
         }
         const yPos = getYPos(-10, 80)
-        // const yPos = Math.random() * ((config.height / 2) - 200);
         console.log(yPos)
         console.log(yPos + 425)
         const pole1 = poles.create(1200, yPos, 'pole').setOrigin(0.5, 0.3).setFlip(false, true)
@@ -40,7 +37,6 @@ function create() {
         const pole2 = poles.create(1200, yPos + 450, 'pole').setOrigin(0.5, 0.3);
         pole1.setVelocityX(-150); 
         pole2.setVelocityX(-150); 
-        // poles.setVelocityY(-200)
         pole1.body.setAllowGravity(false)
         pole1.body.setImmovable(true)
         pole1.outOfBoundsKill = true;
@@ -49,11 +45,6 @@ function create() {
         pole2.outOfBoundsKill = true;
         gameState.poleTop = pole1
         gameState.poleBottom = pole2
-        // poles.setCollideWorldBounds(true);
-        // if (pole.x <= gameState.player.x) {
-        //   gameState.score += 1;
-        //   gameState.scoreText.setText(`Score: ${gameState.score}`)
-        // }
     };
     const poleLoop = this.time.addEvent({
         delay: 1500,
@@ -61,32 +52,6 @@ function create() {
         callbackScope: this,
         loop: true,
       });
-
-      // gameState.checkScore = function() {
-      //   for (let i = 0; poles[i].x < gameState.player.x; i += 1) {
-      //     gameState.score += 1;
-      //     gameState.scoreText.setText(`Score: ${gameState.score}`)
-      //   }
-      // }
-    // function scoreCheck() {
-    //     for (let i = 0; i < gameState.numPoles; i++) {
-    //      if (gameState.allPoles[i].x < gameState.player.x){
-    //       gameState.score += 1;
-    //       gameState.scoreText.setText(`Score: 69`)
-    //     }
-    //   }
-    // }
-    
-    // const scoreCheckLoop = this.time.addEvent({
-    //   delay: 1,
-    //   callback: scoreCheck,
-    //   callbackScope: this,
-    //   loop: true
-    // });    
-      // if (poles.x = gameState.player.x) {
-      //   gameState.score += 1;
-      //   gameState.scoreText.setText(`Score: ${gameState.score}`)
-      // }
       
     this.physics.add.collider(gameState.player, poles, () => {
       this.physics.pause();
@@ -100,38 +65,13 @@ function create() {
     })
     
     gameState.allPoles = poles.getChildren()
-    // gameState.numPoles = gameState.allPoles.length;
-
-    // gameState.flight = this.add.tween({
-    //   target: gameState.player,
-    //   x: 100,
-    //   ease: 'Linear',
-    //   duration: 1000,
-    //   repeat: 1,
-    //   yoyo: false,
-    //   onStart: function() {
-    //   gameState.player.angle += 1
-    //   },
-    //   onEnd: function(){
-    //     gameState.player.angle += -1
-    //   }
-    // })
     gameState.loopOfPoles = poleLoop
 }
 
 function update() {
     if (gameState.cursors.up.isDown || gameState.cursors.space.isDown) {
         gameState.player.setVelocityY(-100)
-        // gameState.player.setAngle(-20)
     };
-    // gameState.flight.play()
-    // gameState.checkScore()
-    // for (let i = 0; i < gameState.numPoles; i++) {
-      //  if (gameState.allPoles[i].x < gameState.player.x){
-    //     gameState.score += 1;
-    //     gameState.scoreText.setText(`Score: 69`)
-      // }
-    // }
     for (let i = 0; i < gameState.allPoles.length; i++) {
       let pole = gameState.allPoles[i]
       if (pole.x < gameState.player.x && !pole.scored) {
@@ -143,11 +83,6 @@ function update() {
         pole.destroy()
       }
     }
-    // if (gameState.score >= 50) {
-    //   gameState.poleTop.setVelocityX(-200)
-    //   gameState.poleBottom.setVelocityX(-200)
-    //   gameState.loopOfPoles.delay = 1000
-    // }
 }
 
 const config = {
